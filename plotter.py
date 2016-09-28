185cb813b02378a794a4beb926ea85ef8da71cba
Aniq55 <aniqrah@gmail.com>
2016-09-28 17:37:08 +0530

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -1,24 +1,45 @@
 import tkinter as tk
 import matplotlib.pyplot as plt
+import math
 
-root= tk.Tk()
-root.title("Polynomial Graph Plotter")
+
 
 def frange(x, y, jump):
   while x < y:
     yield x
     x += jump
 
+def F(x,P):
+		for t in x:
+			n=0
+			s=0
+			for i in P:
+				s+=i*math.pow(t,n)
+				n+=1
+			yield s
+
 def Plotter():
-	P=list(map(int,entry.get().split(',')))
-	x= list(int(llimit.get())),int(ulimit.get())),0.5)
+	P= list(map(float,entry.get().split(',')))
+	x= list(frange(int(llimit.get()),int(ulimit.get()),0.5))
+	y= list(F(x,P))
+	plt.plot(x,y)
+	plt.show()
 	
 
-
+root= tk.Tk()
+root.title("Polynomial Graph Plotter")
+root.geometry("300x100")
 
 entry = tk.Entry(root, width=40, fg='black' ,bg='white')
 llimit= tk.Entry(root, width=10, fg='black', bg='white')
 ulimit= tk.Entry(root, width=10, fg='black', bg='white')
-plot_button = tk.Button(root,text=b,width=5,relief=rel,command=Plotter,fg='white',bg='black')
+plot_button = tk.Button(root,text="Plot",width=5,command=Plotter,fg='white',bg='black')
+
+entry.pack()
+llimit.pack()
+ulimit.pack()
+plot_button.pack()
+
+
 
 root.mainloop()
